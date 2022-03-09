a879cb594ccf3374358da799021fd743d5bc1dc4
toysmoon <toysmoon@gmail.com>
2022-03-09 16:50:33 +0900

    fix: modal css

--- src/shared/components/Modal/index.tsx
+++ src/shared/components/Modal/index.tsx
@@ -2,7 +2,6 @@ import classNames from 'classnames';
 import React, { FC } from 'react';
 import useModal from 'shared/hooks/useModal';
 import Portal from '../Portal';
-import ModalDim from './ModalDim';
 
 export interface IModalProps {
   isOpen: boolean;
@@ -19,26 +18,48 @@ const Modal: FC<IModalProps> = ({
 }) => {
   const modalClass = useModal(isOpen, { useMinHeight });
 
+  if (!isOpen) {
+    return null;
+  }
+
   return (
     <Portal>
-      {isOpen && <ModalDim onClose={onClose} />}
-      <div className={classNames(defaultModalClass, modalClass)}>
-        {children}
+      <div className={classNames(overlayClass)}>
+        <div className={classNames(backgroundClass)} onClick={onClose} />
+        {/* background div floats on top of overlay
+          as a sibling of modal div, onClick closes modal 
+          */}
+        <div className={classNames(defaultModalClass, modalClass)}>
+          <div className="overflow-y-auto w-full">{children}</div>
+        </div>
       </div>
     </Portal>
   );
 };
 
-const defaultModalClass = [
-  'max-w-xl',
-  'mx-auto',
+const overlayClass = [
   'fixed',
+  'top-0',
   'left-0',
   'right-0',
-  'z-50',
+  'bottom-0',
+  'flex',
+  'items-center',
+  'justify-start',
+  'flex-col-reverse',
+  'z-40',
+];
+
+const backgroundClass = ['absolute', 'top-0', 'bottom-0', 'left-0', 'right-0'];
+
+const defaultModalClass = [
+  'relative',
   'bg-white',
   'rounded-t-3xl',
-  'transition-all',
+  'max-w-xl',
+  'max-height-70',
+  'flex',
+  'w-full',
 ];
 
 export default Modal;
