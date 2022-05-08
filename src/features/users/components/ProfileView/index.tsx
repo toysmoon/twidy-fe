26aecbe13a33d548b444575cc13ca881903f3af9
toysmoon <toysmoon@gmail.com>
2022-05-08 20:48:42 +0900

    공유 화면 수정

--- src/features/users/components/ProfileView/index.tsx
+++ src/features/users/components/ProfileView/index.tsx
@@ -1,7 +1,5 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC } from 'react';
 import { User } from 'shared/api/types';
-import { Copy } from 'shared/components/Icons';
-import useToast from 'shared/hooks/useToast';
 import Avatar from '../Profile/Avatar';
 import Name from '../Profile/Name';
 
@@ -10,32 +8,12 @@ interface ProfileProps {
 }
 
 const ProfileView: FC<ProfileProps> = ({ user }) => {
-  const toast = useToast();
-  const url = `twidy.app/${user?.userName}`;
-
-  const handleCopy = useCallback(async () => {
-    try {
-      await navigator.clipboard.writeText(`https://${url}`);
-      toast('Copied to clipboard');
-    } catch {}
-  }, [toast]);
-
   return (
-    <div className="flex flex-col items-center py-10">
+    <div className="flex flex-col gap-4 items-center pt-16 pb-6">
       <Avatar
-        src={
-          user?.profileImageUrl ??
-          'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'
-        }
+        src={user?.profileImageUrl ?? 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'}
       />
       <Name>{`${user?.name}'s Likes`}</Name>
-      <div
-        onClick={handleCopy}
-        className="flex justify-center items-center opacity-50"
-      >
-        <p className=" text-base text-white mr-1">{url}</p>
-        <Copy />
-      </div>
     </div>
   );
 };
