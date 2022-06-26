c61e5182c71c8a7c4c342aaa8f55aba54b956e55
toysmoon <toysmoon@gmail.com>
2022-06-26 17:25:27 +0900

    favicon 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -93,6 +93,10 @@ function TwidyMeta() {
         type="text/css"
         href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css"
       />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 }
