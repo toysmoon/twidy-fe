7e48223da1108d5960142f196c689af2977742aa
toysmoon <toysmoon@gmail.com>
2022-06-26 13:49:43 +0900

    이미지 구현 방식 수정

--- src/shared/components/Templates/Intro/AppImage.tsx
+++ src/shared/components/Templates/Intro/AppImage.tsx
@@ -1,15 +1,7 @@
-import Image from 'shared/components/Image';
-
 export default function AppImage() {
   return (
     <div className="p-10 relative w-full flex justify-center">
-      <Image
-        src={'/images/intro/screen.png'}
-        alt="Picture of the author"
-        layout={'intrinsic'}
-        width={334}
-        height={671}
-      />
+      <img src="/images/intro/screen.png" className="w-80" />
     </div>
   );
 }
