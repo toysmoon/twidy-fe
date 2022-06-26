c1d6217bb5b7f68afeed63bd4196ec8522ca3d46
toysmoon <toysmoon@gmail.com>
2022-06-26 14:04:06 +0900

    최소 높이 지정

--- src/shared/components/Templates/Intro/AppImage.tsx
+++ src/shared/components/Templates/Intro/AppImage.tsx
@@ -1,7 +1,7 @@
 export default function AppImage() {
   return (
     <div className="p-10 relative w-full flex justify-center">
-      <img src="/images/intro/screen.png" className="w-80 min-h-96" />
+      <img src="/images/intro/screen.png" className="w-80 min-h-80" />
     </div>
   );
 }
