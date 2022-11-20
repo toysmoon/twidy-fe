22fa427a8b34c405af9db2bd27a62e0ea3d24f38
toysmoon <toysmoon@gmail.com>
2022-11-20 14:51:26 +0900

    탭 디자인 변경

--- src/shared/components/LikeTab/index.tsx
+++ src/shared/components/LikeTab/index.tsx
@@ -24,26 +24,30 @@ interface ILikeTab {
 export default function LikeTab({ isCollections }: ILikeTab) {
   const router = useRouter();
   const selectedTab = isCollections ? TAB_ITEM.SAVED : TAB_ITEM.MY_LIKES;
-  const tablistClass = classNames(
-    'h-10 flex justify-center items-center rounded-full p-1 bg-tab'
-  );
+  const tablistClass = classNames('flex gap-6');
 
   return (
-    <nav className="flex justify-center my-5">
+    <nav className="mx-4 pt-4">
       <ul className={tablistClass}>
-        {tablist.map((tab) => (
+        {tablist.map(tab => (
           <li
-            key={`tabitem-${tab}`}
+            key={`tab-item-${tab}`}
             onClick={() => router.replace(linkByTabItem[tab])}
-            className={classNames(
-              selectedTab === tab ? 'bg-white' : 'bg-transparent',
-              'w-28 h-8 flex justify-center items-center rounded-full text-h4'
-            )}
+            className={classNames(selectedTab !== tab && 'opacity-50', 'text-h3 text-white')}
           >
             {tab}
+            {selectedTab === tab && (
+              <div
+                className={classNames(
+                  selectedTab === tab ? 'bg-white' : 'bg-transparent',
+                  'w-full h-1 mt-3 rounded-full'
+                )}
+              />
+            )}
           </li>
         ))}
       </ul>
+      <div className="bg-white opacity-50 w-full h-1px -mt-1px" />
     </nav>
   );
 }
