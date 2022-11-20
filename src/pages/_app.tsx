982ce0a651f0f23a178abec0f84fd1640eb73292
toysmoon <toysmoon@gmail.com>
2022-11-20 19:30:51 +0900

    테마색 변경 테스트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -106,12 +106,6 @@ function TwidyMeta() {
       <meta name="twitter:description" content="Tidy up your liked tweets!" />
       <meta name="twitter:image:src" content="https://twidy.app/images/og.png" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 }
--- src/shared/components/Templates/Layout/Container.tsx
+++ src/shared/components/Templates/Layout/Container.tsx
@@ -1,5 +1,7 @@
+import Head from 'next/head';
 import React from 'react';
-import useTheme, { colorList } from 'shared/hooks/useTheme';
+import { useRecoilValue } from 'recoil';
+import { themeState } from 'shared/states/themeState';
 
 interface ContainerProps {
   children: React.ReactNode;
@@ -7,18 +9,31 @@ interface ContainerProps {
 }
 
 export default function Container({ children, color }: ContainerProps) {
-  const theme = useTheme();
-  const primaryColor = color ? colorList[color] : theme;
+  const theme = useRecoilValue(themeState);
+
   return (
     <div className="h-full max-w-xl my-0 mx-auto box-border bg-transparent ">
       {children}
+      <Head>
+        <meta name="theme-color" content={themeColor[color ?? theme]} />
+      </Head>
       <style jsx global>{`
         html,
         body {
-          background-color: ${primaryColor};
-          --colors-primary: ${primaryColor};
+          background-color: ${themeColor[color ?? theme]};
+          --colors-primary: ${themeColor[color ?? theme]};
         }
       `}</style>
     </div>
   );
 }
+
+const themeColor: Record<string, string> = {
+  black: '#000000',
+  violet: '#745aff',
+  twitter: '#1da1f2',
+  mint: '#00c2b7',
+  orange: '#ff9f1c',
+  heart: '#ff508f',
+  yellow: '#ffdc23',
+};
